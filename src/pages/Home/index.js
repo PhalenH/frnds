da551fc8759a7625dc8170b9827fe6c57220ceac
PhalenH <pchaze@yahoo.com>
2022-05-05 18:08:52 -0400

    wrote ideas on homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 export default function Home() {
-  return (<div>
-    FRNDS
-  </div>);
+  return (
+    <div>
+      <h1>FRNDS</h1>
+    {/* This homepage will likely have who they are, the mission statement, etc.
+    looking to create possibly a scrollable page or have multiple pages like:
+    home, past events, future events, about, team, collaborations, ways to reach out */}
+    </div>
+  );
 }
