7901b0c88242290a1c3d510ff069ee975e6a35b0
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-19 21:00:41 -0400

    added navbard to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        // className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/profile"
+        // className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Profile
+      </NavLink>
+      {/* <NavLink
+        to="/products"
+        // className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to="/login"
+        // className={({ isActive }) => (isActive ? "link active" : "link")}
+      >
+        Login
+      </NavLink> */}
+    </div>
+  );
 }
