150227bb80e4e65adb58b8d3696b434a2f891c45
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-05-24 18:24:41 -0400

    added to signup/login logic

--- src/pages/Home/Signup/index.js
+++ src/pages/Home/Signup/index.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { useMutation } from "@apollo/client";
 
-export default function index() {
-  return (
-    <div>Signup</div>
-  )
+export default function Login() {
+  const [formState, setFormState] = useState({
+    name: "",
+    password: "",
+  });
+  const [addProfile, { error, data }] = useMutation();
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addProfile({
+        variables: { ...formState },
+      });
+
+      //   Auth.login(data.addProfile.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  return <div>Signup</div>;
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+// import { LOGIN_USER } from "../utils/mutations";
 
-export default function index() {
-  return (
-    <div>Login</div>
-  )
+export default function Login() {
+  const [formState, setFormState] = useState({
+    name: "",
+    password: "",
+  });
+  const [login, { error, data }] = useMutation();
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // submit form
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+
+    //   Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      name: "",
+      password: "",
+    });
+  };
+  return <div>Login</div>;
 }
