d2880f672d796705a61a8e4df454e7ad27be83db
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-06-23 19:59:53 -0400

    added page to be used in homepage to test pagination with mock data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Posts } from "../Posts";
 
 export default function Home() {
-  return <div>
-    
-  </div>;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setcurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-white mb-3">Testing Pagination</h1>
+      <Posts posts={posts} loading={loading} />
+    </div>
+  );
 }
